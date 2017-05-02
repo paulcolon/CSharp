6b43539dcb1d5d5dc7eaaf075c0ae343238bb2ea
paulcolon <pdcolon@gmail.com>
2017-05-02 12:41:03 -0400

    enter how many minutes you exercised

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -8,6 +8,7 @@ namespace FitnessFrog
         public static void Main(string[] args)
         {
             // Prompt the user for minutes exercised
+            Console.Write("Enter how many minutes you exercised");
             // Add minutes exercised to total
             // Display total minutes exercised to the screen
             // Repeat until the user quits
