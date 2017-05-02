6f7b51b87e24b71bd84bd823bd49617a96e143a5
paulcolon <pdcolon@gmail.com>
2017-05-02 12:15:55 -0400

    notes added as to do list

---
+++ FitnessFrog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessFrog
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Prompt the user for minutes exercised
+            // Add minutes exercised to total
+            // Display total minutes exercised to the screen
+            // Repeat until the user quits
+
+        }
+    }
+}
