265c8febfe87728d79bc8fc25d06ca64d08eb7cb
paulcolon <pdcolon@gmail.com>
2017-05-01 22:56:39 -0400

    uload files 1st time

---
+++ Hello World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Press Any Key to Cont");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Name/Name/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Name
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! :)");
+            Console.WriteLine("Press  Any Key to Continue...");
+            Console.RaedKey();
+
+        }
+    }
+}
