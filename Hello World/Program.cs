846ef7653ff2f6d89eb1033fd3adf86062d4caba
paulcolon <pdcolon@gmail.com>
2017-05-02 10:05:15 -0400

    hello world reloaded

---
+++ Hello World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key continue");
+            Console.ReadKey();
+        }
+    }
+}
