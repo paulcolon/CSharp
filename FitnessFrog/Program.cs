c69ed9e1ae52df33b46cc6c96544bdd03085185d
paulcolon <pdcolon@gmail.com>
2017-05-09 13:09:32 -0400

    if / else if / else

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -25,9 +25,27 @@ namespace FitnessFrog
                 {
 
                     var minutes = int.Parse(entry);
+
+                    if (minutes <= 10)
+                    {
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go hot stuff!");
+                    }
+                       else if (minutes <= 60)
+                    {
+                        Console.WriteLine("You must be a ninja warrior in training!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Okay, now you're just showing off!");
+                    }
+                    // Add minutes exercised to total
                     runningTotal = runningTotal + minutes;
 
-                    // Add minutes exercised to total
+
                     // Display total minutes exercised to the screen
                     Console.WriteLine("You've enterd" + runningTotal + "minutes");
 
