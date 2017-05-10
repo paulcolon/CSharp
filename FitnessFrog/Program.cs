e9ba6e4dab81666a7f28cec753e117d609530cd3
paulcolon <pdcolon@gmail.com>
2017-05-10 12:53:32 -0400

    try code and catch exceptions

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -23,42 +23,48 @@ namespace FitnessFrog
                 }
                 else
                 {
-
-                    var minutes = int.Parse(entry);
-
-                    if (minutes <= 0)
+                    try
                     {
-                        Console.WriteLine(minutes + "is not an acceptable value");
+                        // Add minutes exercised to total
+                        int minutes = int.Parse(entry);
+
+                        if(minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+                        else if(minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if(minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go hot stuff!");
+                        }
+                        else if(minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
+
+                        runningTotal = runningTotal + minutes;
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
                         continue;
                     }
 
-                    if (minutes <= 10)
-                    {
-                        Console.WriteLine("Better than nothing, am I right?");
-                    }
-                    else if (minutes <= 30)
-                    {
-                        Console.WriteLine("Way to go hot stuff!");
-                    }
-                       else if (minutes <= 60)
-                    {
-                        Console.WriteLine("You must be a ninja warrior in training!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Okay, now you're just showing off!");
-                    }
-                    // Add minutes exercised to total
-                    runningTotal = runningTotal + minutes;
-
-
                     // Display total minutes exercised to the screen
-                    Console.WriteLine("You've enterd" + runningTotal + "minutes");
-
-                    // Repeat until the user quits
+                    Console.WriteLine("You've entered " + runningTotal + " minutes.");
                 }
-                Console.WriteLine("Goodbye");
+                // Repeat until user quits
             }
+
+            Console.WriteLine("Goodbye");
         }
     }
 }
