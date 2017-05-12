0d4a16938a0391771246f1fa223c50595802b958
paulcolon <pdcolon@gmail.com>
2017-05-12 14:43:15 -0400

    objects0 files created for project in lesson

---
+++ objects0/objects0/Tower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace objects0
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
