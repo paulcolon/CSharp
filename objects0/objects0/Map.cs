db07b96dea8081d5c77880ed7f144acb1f2230f9
paulcolon <pdcolon@gmail.com>
2017-05-16 15:35:10 -0400

    object initialzation

--- objects0/objects0/Map.cs
+++ objects0/objects0/Map.cs
@@ -2,7 +2,13 @@
 {
     public class Map
     {
-        public int Width;
-        public int Height;
+        public readonly int Width;
+        public readonly int Height;
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
     }
 }
