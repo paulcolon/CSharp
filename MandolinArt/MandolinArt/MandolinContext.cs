7c7f695f0306b120da7ae726dc5473edfe44b6b4
paulcolon <pdcolon@gmail.com>
2017-07-10 23:29:53 -0400

    index update

---
+++ MandolinArt/MandolinArt/MandolinContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using MandolinArt.Models;
+
+namespace MandolinArt
+{
+    public class MandolinContext : DbContext
+    {
+            public DbSet<Mandolin> Mandolins { get; set; }
+    }
+}
---
+++ MandolinArt/MandolinArt/Models/Mandolin.cs
@@ -0,0 +1,14 @@
+﻿namespace MandolinArt.Models
+{
+    public class Mandolin
+    {
+        public int ID { get; set; }
+        public string Maker { get; set; }
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public decimal Value { get; set; }
+        public byte[] Picture { get; set; }
+       
+    }
+    
+}
